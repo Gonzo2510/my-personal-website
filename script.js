39c2023f4ad2393d5191ec0367107a51227be03c
jacobgonzales <jagonz@live.com>
2023-05-02 22:00:20 -0500

    made button clickable to scroll to top

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const h2 = document.createElement("h2");
 h2.textContent = "I am currently employed as a teacher, but am looking to switch careers. I want to become a software engineer. In order to accomplish my goal I have enrolled in flatiron school. I am very excited about what I am learning and I can't wait to tackle new problems with my newfound abilities."
 
 //document.querySelector("body").appendChild(h2);
+
+
+
+let myButton = document.getElementById("whiteSpace");
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
